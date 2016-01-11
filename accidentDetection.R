e0a443b5ad2c853cef0d2dd61ee2152cfd6fdb04
unknown <abhinav.sunderrajan@CE-2011-083.TUMCREATE.LOCAL>
2016-01-11 10:37:03 +0800

    trajectory QI dataset

---
+++ accidentDetection.R
@@ -0,0 +1,45 @@
+library(RPostgreSQL)
+library(ggplot2)
+require(gridExtra)
+library("plot3D")
+require(tree)
+library(caret)
+library(partykit)
+library("reshape2")
+library(rpart)
+library(doParallel)
+
+drv <- dbDriver("PostgreSQL")
+con <- dbConnect(drv, host='172.25.187.111', port='5432', dbname='abhinav',
+                 user='abhinav', password='qwert$$123')
+
+
+day=21
+month=9
+hours="(17,18)"
+
+
+
+query=paste("select track_info_id,road_id,segment,speed,time_stamp,day,month,hour,minute,distance_along_road from map_matched_express_ways where day=",day 
+," and month=",month," and hour in ",hours, "and segment <>-1 and road_id in(30634,30635,30636,38541,30637,30638,30639,30640,30641,37981,30642,30643,38539,30644,30645,30646,30647,30648,30649,30650,30651,30580,30581) order by time_stamp",sep="")
+
+print(query)
+
+data<-dbGetQuery(con,query)
+
+#data=data[data$track_info_id%%2==0,]
+
+data$track_info_id=as.factor(data$track_info_id)
+data$time=as.POSIXct(data$time_stamp/1000, origin="1970-01-01")
+
+
+p <- ggplot(data, aes(x=time, y=distance_along_road, group=track_info_id, colour = track_info_id))
+p + geom_point() +geom_line()
+
+#+ geom_text(aes(label=time),hjust=0, vjust=0)
+
+
+#Close Database connection
+dbDisconnect(con)
+
+
--- anomalydetection.R
+++ anomalydetection.R
@@ -1,16 +1,140 @@
-library(AnomalyDetection)
+library(RPostgreSQL)
 library(ggplot2)
-help(AnomalyDetectionTs)
-data(raw_data)
-summary(raw_data)
+require(gridExtra)
+library("plot3D")
+require(tree)
+library(caret)
+library(partykit)
+library("reshape2")
+library(rpart)
+library(doParallel)
 
-pd <- position_dodge(.1)
-g<-ggplot(aes(x=timestamp,y=count,ymax=max(count)*1.05),data=raw_data)+
-  labs(x="Time", y="count", title="Count vs Time")
-g<-g+geom_line(position=pd,size=1,colour = "#4a6084")+
-  geom_point(position=pd,size=2,,colour = "#4a6084")+ylim(21, 255)+
-  theme_bw()
 
-g
-res = AnomalyDetectionTs(raw_data, max_anoms=0.02, direction='both', plot=TRUE)
-res$plot
+drv <- dbDriver("PostgreSQL")
+con <- dbConnect(drv, host='172.25.187.111', port='5432', dbname='abhinav',
+                 user='abhinav', password='qwert$$123')
+
+      times1=c(1600)
+      times2=times1+120
+      penetration=1.0
+
+      pieModels=list()
+
+
+        for(time in 1:1){
+            time1=times1[time]
+            time2=times2[time]
+            
+            query<-paste("select distance_along_road as distance,speed,time_stamp,agent_id from semsim_output WHERE iteration_count=51  AND time_stamp >=",time1, " AND time_stamp <=",time2," AND distance_along_road<=13000", sep="")
+            training<-dbGetQuery(con,query)
+            
+            
+            #Decision tree rpart
+            set.seed(108)
+            
+            modelFit <- rpart(speed ~ distance,data=training,control=rpart.control(maxdepth=15))
+            #rpart1a <- as.party(modelFit)
+            #plot(rpart1a)
+         
+            index=1
+            decisonTreeSplit=numeric()
+            for(split in modelFit$splits[,4]){
+                  decisonTreeSplit[index]=split
+                  index=index+1
+            }
+            
+            decisonTreeSplit<-sort(decisonTreeSplit)
+            decisonTreeSplit<-c(0.0,decisonTreeSplit,max(training$distance))
+            
+            cutDT<-cut(training$distance,breaks = decisonTreeSplit,labels = decisonTreeSplit[1:length(decisonTreeSplit)-1])
+            distance_split<-split(training,cutDT)            
+           
+            ls_density=numeric()
+            ls_speed=numeric()
+            speedList=list()
+            
+            i=1
+            
+            for(name_dist in names(distance_split)){
+                  y<-distance_split[[name_dist]]
+                  c2<-cut(y$time_stamp,breaks =  seq(time1, time2, by = 1),labels = seq(time1, (time2-1), by = 1))
+                  time_split<-split(y,c2)
+                  density_vec=numeric()
+                  speed_vec=numeric()
+                  index=1
+                  for(name_time in names(time_split)){
+                        
+                        num_of_vehicles<-length(unique(time_split[[name_time]]$agent_id))*(1/penetration)
+                        density_vec[index]=num_of_vehicles*1000.0/(decisonTreeSplit[i+1]-decisonTreeSplit[i])
+                        speed_vec[index]=mean(time_split[[name_time]]$speed)
+                        index=index+1
+                  }
+                  
+                  good=complete.cases(speed_vec,density_vec)
+                  speed_vec=speed_vec[good]
+                  density_vec=density_vec[good]
+                  
+                  good=speed_vec>0
+                  speed_vec=speed_vec[good]
+                  density_vec=density_vec[good]
+                  
+                  
+                  ls_density=c(ls_density,density_vec)
+                  ls_speed=c(ls_speed,speed_vec)
+                  i=i+1
+                  
+                  speedList[[name_dist]]=c(speedList[[name_dist]],speed_vec)
+                  
+            }
+            
+            good=complete.cases(ls_speed,ls_density)
+            ls_speed=ls_speed[good]
+            ls_density=ls_density[good]
+            
+            good=ls_speed>0
+            ls_speed=ls_speed[good]
+            ls_density=ls_density[good]
+            fit=lm(ls_density~log(ls_speed))
+            
+            summary(fit)$r.squared 
+            
+            
+            
+            densityList=list()
+            flowList=list()
+            
+            for(name in names(speedList)){
+                  newData=data.frame(speedList[[name]])
+                  colnames(newData)=c("ls_speed")
+                  densityList[[name]]=predict(fit,newdata=newData)
+            }
+            
+            for(name in names(speedList)){
+                  flowList[[name]]=speedList[[name]]* densityList[[name]]*3.6
+                  
+            }
+            
+            flow1=lapply(flowList,mean)
+            density1=lapply(densityList,mean)
+            speed1=lapply(speedList,mean)
+            
+            
+            d=data.frame(as.numeric(flow1),as.numeric(speed1),as.numeric(density1),row.names=names(speedList))
+            colnames(d)=c("Flow","Speed","Density")
+            pieModels[[toString(time)]]=d
+            print(summary(d))
+            print(paste("Finished model ",time,sep=""))
+            
+        }
+
+      
+      
+      
+      
+      
+      
+      
+      #Close Database connection
+      dbDisconnect(con)
+      
+      
